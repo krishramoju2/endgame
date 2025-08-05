9eb1724d832022493fc9bcb2642f21716a23eeac
krishramoju2 <futureai.incorporated@gmail.com>
2025-08-05 22:32:25 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,21 @@ export default function Home() {
   const { careers, setCurrentCareerId } = useAppContext();
 
   return (
+    <Layout>
+      <div style={{ padding: "2rem" }}>
+        <h1>Welcome to CareerOS</h1>
+        <p>Choose a career expert to begin.</p>
 
-
-        Welcome to CareerOS
-        Choose a career expert to begin.
-
-
-
-        {careers.map((career) => (
-           setCurrentCareerId(career.id)}
-          />
-        ))}
-
-
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem", marginTop: "2rem" }}>
+          {careers.map((career) => (
+            <CareerCard
+              key={career.id}
+              career={career}
+              onClick={() => setCurrentCareerId(career.id)}
+            />
+          ))}
+        </div>
+      </div>
+    </Layout>
   );
 }
